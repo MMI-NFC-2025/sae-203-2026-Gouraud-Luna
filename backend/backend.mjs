3c86bea10415d0028a414ee7465c2439adccbbb5
Gouraud Luna <luna.gouraud@edu.univ-fcomte.fr>
2026-03-12 10:06:09 +0100

    btn info dans index

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-export const pb = new PocketBase('http://127.0.0.1:8090') ;
+export const pb = new PocketBase('https://flyingtapfest.luna-gouraud.fr:443') ;
 
 export async function allArtiste() {
 const records = await pb.collection('Artistes').getFullList();
